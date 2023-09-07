ee4e5513b9b3778ca0fd043434393eb366650445
Philip Kvam <57698186+phikva@users.noreply.github.com>
2023-09-07 19:51:14 +0200

    fixed import

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/apolloClient";
 import Layout from "../components/layouts/Layout"; // Import the Layout component
 import "../styles/tailwind.css";
-import { SeoProvider } from "../context/seo";
+import { SeoProvider } from "../context/Seo";
 
 function MyApp({ Component, pageProps }) {
   const client = useApollo(pageProps.initialApolloState);
