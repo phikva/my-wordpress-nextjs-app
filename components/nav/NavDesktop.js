0a9320b5c39c16a55d1da651d0a44fa4afbc0f0e
Philip Kvam <57698186+phikva@users.noreply.github.com>
2023-09-10 23:46:16 +0200

    Added Next link to nav

--- components/nav/NavDesktop.js
+++ components/nav/NavDesktop.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link"; // Import Link from Next.js
 
 function NavDesktop({ menuItems }) {
   return (
@@ -6,12 +7,12 @@ function NavDesktop({ menuItems }) {
       <ul className="container mx-auto flex flex-wrap justify-center space-x-4">
         {menuItems.map((menuItem, index) => (
           <li key={index}>
-            <a
-              href={menuItem.url}
-              className="text-gray-300 hover:text-white transition duration-300"
-            >
-              {menuItem.label}
-            </a>
+            {/* Use Link component for client-side routing */}
+            <Link className="text-gray-300 hover:text-white transition duration-300" href={menuItem.url}>
+             
+                {menuItem.label}
+         
+            </Link>
           </li>
         ))}
       </ul>
--- components/nav/NavMobile.js
+++ components/nav/NavMobile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DynamicIcon from "../modules/DynamicIcon";
+import Link from "next/link"; // Import Link from Next.js
 
 function NavMobile({ menuItems, onClose }) {
   return (
@@ -8,12 +9,12 @@ function NavMobile({ menuItems, onClose }) {
       <ul className="container mx-auto mt-10 flex flex-col space-y-4">
         {menuItems.map((menuItem, index) => (
           <li key={index}>
-            <a
-              href={menuItem.url}
-              className="text-gray-300 hover:text-white transition duration-300"
-            >
-              {menuItem.label}
-            </a>
+            {/* Use Link component for client-side routing */}
+            <Link className="text-gray-300 hover:text-white transition duration-300" href={menuItem.url}>
+             
+                {menuItem.label}
+             
+            </Link>
           </li>
         ))}
       </ul>
