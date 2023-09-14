6d1144789117c6ac9cbb165d667cac1c9d43d962
Philip Kvam <57698186+phikva@users.noreply.github.com>
2023-09-14 09:58:18 +0200

    merged header and menu query together. fix header

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -1,40 +1,46 @@
 import React, { useState, useEffect } from "react";
-
-// seo related
 import SeoHead from "../seoHead/SeoHead";
 import { useSeo } from "../../context/Seo";
 import Link from "next/link";
-
-// import ui components
+//import ui components
 import Button from "../ui/Button";
 import HamburgerButton from "../ui/HamburgerButton";
-
-// import nav components
+//import components
 import NavDesktop from "../nav/NavDesktop";
 import NavMobile from "../nav/NavMobile";
-
+import CustomImage from "../modules/CustomImage";
 // import helper functions
 import { toggleMobileMenu } from "../../utils/menuUtils";
 import { handleResize } from "../../utils/windowUtils";
 import { getSelectedLogo } from "../../utils/selectLogoUtils";
 
-// import apollo client
-import { useQuery } from "@apollo/client";
-
-//get menu data from query
-import GetMenuData from "../../graphql/GetMenuData.graphql";
-import CustomImage from "../modules/CustomImage";
 
 function Header({ headerData }) {
   const { seoData } = useSeo();
   const header = headerData?.pageBy.header || {};
-  const menuSelection = header.menuSelection || "";
-  // get menu data from query
-  const { data, loading, error } = useQuery(GetMenuData);
+  const menuSelection = header.menuSelection || [];
+  const menus = headerData?.menus?.nodes || [];
 
   // State to track mobile menu visibility
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    handleResize(setIsMobileView);
+  }, []);
+
+  // helper function to toggle mobile menu
+  const toggleMobileMenuHandler = () => {
+    toggleMobileMenu(isMobileMenuOpen, setIsMobileMenuOpen);
+  };
+    // helper function to select logo
+    const { selectedLogoHeader, altTextHeader } = getSelectedLogo(headerData, {});
+
+  const selectedMenu = menus.find((menu) => menu.name === menuSelection);
+
+  if (!selectedMenu) {
+    console.log("No menu found for the selected menuSelection.");
+  }
 
   // seo related
   const seoTitle = header.pageTitle || (seoData ? seoData.defaultTitle : "");
@@ -48,99 +54,63 @@ function Header({ headerData }) {
   // Define primaryButton data or null if not available
   const primaryButton = shouldDisplayButton ? header.headerCta : null;
 
-  // helper function to select logo
-  const { selectedLogoHeader, altTextHeader } = getSelectedLogo(headerData,{});
 
-  // helper function to determine if the window is mobile
-  useEffect(() => {
-    handleResize(setIsMobileView);
-  }, []);
 
-  // helper function to toggle mobile menu
-  const toggleMobileMenuHandler = () => {
-    toggleMobileMenu(isMobileMenuOpen, setIsMobileMenuOpen);
-  };
+  return (
+    <header className="bg-white-light sticky top-0 border-b border-gray-light text-white flex justify-center">
+      <SeoHead
+        title={seoTagline ? `${seoTitle} - ${seoTagline}` : seoTitle}
+        description={seoDescription}
+      />
+      <div className="flex md:flex-row justify-between items-center w-full p-5 max-w-7xl">
+        {selectedLogoHeader && (
+          <Link href="/">
+            <CustomImage
+              src={selectedLogoHeader}
+              alt={altTextHeader}
+              className={`cursor-pointer ${
+                isMobileView ? "w-16 h-8" : "w-20 h-10"
+              }`}
+              width={isMobileView ? 130 : 150}
+              height={isMobileView ? 100 : 100}
+            />
+          </Link>
+        )}
 
-  let content = null;
-
-  if (loading) {
-    // Handle loading state
-    content = <p>Loading menu...</p>;
-  } else if (error) {
-    // Handle error state
-    console.error("GraphQL Error:", error);
-    content = <p>Error loading data.</p>;
-  } else {
-    const menus = data?.menus?.nodes || [];
-
-    // Find the menu based on menuSelection
-    const selectedMenu = menus.find((menu) => menu.name === menuSelection);
-
-    if (!selectedMenu) {
-      // Handle the case where the menuSelection doesn't match any menu
-      content = <p>No menu found for the selected menuSelection.</p>;
-    } else {
-      const menuItems = selectedMenu.menuItems?.nodes || [];
-
-      content = (
-        <header className="bg-white-light sticky top-0 border-b border-gray-light text-white flex justify-center">
-          <SeoHead
-            title={seoTagline ? `${seoTitle} - ${seoTagline}` : seoTitle}
-            description={seoDescription}
-          />
-          <div className="flex md:flex-row justify-between items-center w-full p-5 max-w-7xl">
-            {selectedLogoHeader && (
-              <Link href="/">
-                <CustomImage
-                  src={selectedLogoHeader}
-                  alt={altTextHeader}
-                  className={`cursor-pointer ${
-                    isMobileView ? "w-16 h-8" : "w-20 h-10"
-                  }`}
-                  width={isMobileView ? 130 : 150}
-                  height={isMobileView ? 100 : 100}
-                />
-              </Link>
+        {menuSelection && (
+          <div
+            className={`md:text-center order-last  ${
+              isMobileView ? "md:w-full" : "md:w-2/5"
+            }`}
+          >
+            {/* Menu */}
+            {isMobileView ? (
+              <HamburgerButton onClick={toggleMobileMenuHandler} />
+            ) : (
+              <NavDesktop menuItems={selectedMenu.menuItems.nodes} />
             )}
-
-            {menuSelection && (
-              <div
-                className={`md:text-center order-last  ${
-                  isMobileView ? "md:w-full" : "md:w-2/5"
-                }`}
-              >
-                {/* Menu */}
-                {isMobileView ? (
-                  <HamburgerButton onClick={toggleMobileMenuHandler} />
-                ) : (
-                  <NavDesktop menuItems={menuItems} />
-                )}
-                {isMobileMenuOpen && isMobileView && (
-                  <NavMobile
-                    menuItems={menuItems}
-                    onClose={toggleMobileMenuHandler}
-                  />
-                )}
-              </div>
-            )}
-
-            {primaryButton && (
-              <div className="order-2 md:order-last">
-                {/* Header Button */}
-                <Button
-                  label={primaryButton.buttonText}
-                  link={primaryButton}
-                  isHeaderButton={true}
-                />
-              </div>
+            {isMobileMenuOpen && isMobileView && (
+              <NavMobile
+                menuItems={selectedMenu.menuItems.nodes}
+                onClose={toggleMobileMenuHandler}
+              />
             )}
           </div>
-        </header>
-      );
-    }
-  }
+        )}
 
-  return content;
+        {primaryButton && (
+          <div className="order-2 md:order-last">
+            {/* Header Button */}
+            <Button
+              label={primaryButton.buttonText}
+              link={primaryButton}
+              isHeaderButton={true}
+            />
+          </div>
+        )}
+      </div>
+    </header>
+  );
 }
 
 export default Header;
